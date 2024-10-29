95cbce0b023132186dc5341831cb3728816d7b37
fertorres05 <fernandotorresserra05@gmail.com>
2024-10-29 14:01:51 +0100

    v1.0

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -1,12 +1,23 @@
 package com.example.imcapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.internal.ViewUtils.getBackgroundColor
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    //Definimos los CardView como Clases
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+
+    var isMaleSelected:Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,53 @@ class ImcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        initListeners()
+
     }
+
+    //Inicializamos los card view
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    //Definimos los Listeners
+    fun initListeners(){
+        viewMale.setOnClickListener {
+            isMaleSelected = true
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            isMaleSelected = false
+            setGenderColor()
+        }
+
+    }
+
+    //Definimos el color de el fondo
+    fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+
+    }
+
+
+    fun getBackGroundColor(isComponentSelected:Boolean): Int {
+        //Comprueba si el componente esta seleccionado
+        val colorReference = if(isComponentSelected) {
+            R.color.bg_comp_sel
+        } else {
+            R.color.bg_comp
+        }
+        return ContextCompat.getColor(this,colorReference)
+    }
+
+    fun initUI(){
+        setGenderColor()
+    }
+
+
+
 }
